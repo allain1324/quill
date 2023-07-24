8dd0b01eb7102f718a95c9f92c54888e90139a21
allain1324 <taibanhatk1@gmail.com>
2023-07-24 14:02:53 +0700

    update

--- formats/size.ts
+++ formats/size.ts
@@ -2,11 +2,11 @@ import { ClassAttributor, Scope, StyleAttributor } from 'parchment';
 
 const SizeClass = new ClassAttributor('size', 'ql-size', {
   scope: Scope.INLINE,
-  whitelist: ['small', 'large', 'huge', 'medium'],
+  whitelist: ['8', '9', '10', '11', '12', '14', '18', '24', '30', '36', '48', '60', '72', '96'],
 });
 const SizeStyle = new StyleAttributor('size', 'font-size', {
   scope: Scope.INLINE,
-  whitelist: ['10px', '18px', '32px', '20px'],
+  whitelist: ['10px'],
 });
 
 export { SizeClass, SizeStyle };
