fd04906344e09641d16b501c432620c56bbf444a
allain1324 <taibanhatk1@gmail.com>
2023-07-31 16:01:56 +0700

    update

--- formats/figure.ts
+++ formats/figure.ts
@@ -2,7 +2,7 @@ import { EmbedBlot } from 'parchment';
 
 class Figure extends EmbedBlot {
   static blotName = 'figure';
-  static tagName = 'IMG';
+  static tagName = 'FIGURE';
 
   static create(value) {
     const domNode = super.create(value) as Element;
