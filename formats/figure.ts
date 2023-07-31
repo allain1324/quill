e0b8dd47d3b25bce1ebacbf58059438032f523e3
allain1324 <taibanhatk1@gmail.com>
2023-07-31 16:53:36 +0700

    update

--- formats/figure.ts
+++ formats/figure.ts
@@ -1,13 +1,20 @@
+import { LinkedList } from 'parchment';
 import Block from '../blots/block';
+
+import Image from './image';
 
 class Figure extends Block {
   static blotName = 'figure';
   static tagName = 'FIGURE';
 
+  children: LinkedList<Image>;
+
   static create(value) {
     const domNode = super.create(value) as Element;
     return domNode;
   }
+
+
 }
 
 export default Figure;
