3ad975f10f6ff60dac3e089d36cc8d62c3a1611a
allain1324 <taibanhatk1@gmail.com>
2023-07-31 09:56:32 +0700

    update

--- modules/imageResize.ts
+++ modules/imageResize.ts
@@ -101,7 +101,7 @@ class ImageResize extends Module {
     // update image size
     const deltaX = event.clientX - this.dragStartX;
     // console.log("handleDrag", this.dragCorner, this.corners, this.dragCorner === this.corners[3])
-    if (this.dragCorner === this.corners[3]) {
+    if (this.dragCorner === this.corners[2] || this.dragCorner === this.corners[5]) {
       console.log('detalX', deltaX)
       return;
     }
