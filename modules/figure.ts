f1171bee5539e2db3c7fa796a29a4942a67fa79f
allain1324 <taibanhatk1@gmail.com>
2023-08-01 16:10:24 +0700

    update

---
+++ modules/figure.ts
@@ -0,0 +1,16 @@
+import Quill from "../core/quill";
+import Module from '../core/module';
+
+class Figure extends Module {
+
+  private media?: HTMLElement;
+
+  constructor(quill: Quill) {
+    super(quill);
+    
+  }
+
+
+}
+
+export default Figure;
--- modules/imageResize.ts
+++ modules/imageResize.ts
@@ -15,9 +15,8 @@ class ImageResize extends Module {
 
 
   constructor(quill: Quill) {
-    console.log("modules/imageResize",)
     super(quill);
-    this.quill.root.addEventListener('click', this.handleClick, false);
+    this.quill.root.addEventListener('mouseover', this.handleClick, false);
     this.parentNode = this.quill.root.parentNode as HTMLElement;
     this.parentNode.style.position =
       this.parentNode.style.position || "relative";
