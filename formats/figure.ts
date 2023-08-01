b6ee771c1b73f292d2c8f26ebee0b78dc49c4b2f
allain1324 <taibanhatk1@gmail.com>
2023-08-01 15:27:15 +0700

    update

--- formats/figure.ts
+++ formats/figure.ts
@@ -15,7 +15,7 @@ class Figcaption extends Block {
 
   static create(value) {
     console.log("value figcaption", value);
-    const domNode = super.create() as Element;
+    const domNode = super.create(value) as Element;
     if(typeof value === 'string') {
       domNode.innerHTML = value;
     }    
@@ -34,7 +34,7 @@ class Figure extends Block {
   children: LinkedList<Figcaption>;
 
   static create(value) {
-    const domNode = super.create() as Element;
+    const domNode = super.create(value) as Element;
     console.log("value figure", value);
     if (typeof value === 'object') {
       const imageNode = Image.create(value.image);
