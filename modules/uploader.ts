e72e0805a5bf7aa123f8760afd413cfef1f73267
allain1324 <taibanhatk1@gmail.com>
2023-08-01 10:19:24 +0700

    update

--- modules/uploader.ts
+++ modules/uploader.ts
@@ -70,9 +70,7 @@ Uploader.DEFAULTS = {
     Promise.all(promises).then(images => {
       const update = images.reduce((delta: Delta, image) => {
         console.log("promises image", image);
-        return delta.insert({ attributes: {
-          table: true
-        } });
+        return delta.attributes({ table: true })
       }, new Delta().retain(range.index).delete(range.length)) as Delta;
       this.quill.updateContents(update, Emitter.sources.USER);
       this.quill.setSelection(
